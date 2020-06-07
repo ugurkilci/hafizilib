a34f76fb51e9497a07c6d59acd528b40117e2180
Uğur KILCI <ugurbocugu8@gmail.com>
2020-06-07 16:35:35 +0300

    Create mobil-yonlendirme.php

---
+++ mobil-yonlendirme.php
@@ -0,0 +1,13 @@
+<?php
+
+// Siteye girenler mobilden girdiyse başka şeyler gösterebilirsin. :)
+
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$webos = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$bberry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+if ($iphone || $android || $webos || $ipod || $bberry == true)
+{
+echo "blabla";
+}

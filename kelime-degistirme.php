e3e467815ad9f96721a8287c14ab2be8a0156879
Uğur KILCI <ugurbocugu8@gmail.com>
2020-06-07 16:41:37 +0300

    Create kelime-degistirme.php

---
+++ kelime-degistirme.php
@@ -0,0 +1,7 @@
+<?php
+
+// Kelimeleri değiş tokuş yapıyor. Bununlada çok manyak şeyler yapılabilir. Bkz: BBCode
+
+$sehir = "Aydın Söke de bir çay içelim mi?";
+echo str_replace("çay","Aydın",$sehir);
+//Şehir değişkenindeki çay yerine Aydın yazacak.

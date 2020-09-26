5c5d4455a366c6d0a65e70c5859727871fe35b2c
Uğur KILCI <ugurbocugu8@gmail.com>
2020-09-26 21:51:34 +0300

    Update mobil-yonlendirme.php
    sıkıştırılmış ve blackberry kaldırılmıştır. birileri hala blackberry kullanıyor mu? bana yazsın ekleriz. :)

--- mobil-yonlendirme.php
+++ mobil-yonlendirme.php
@@ -1,13 +1,16 @@
 <?php
 
 // Siteye girenler mobilden girdiyse başka şeyler gösterebilirsin. :)
-
-$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-$webos = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
-$bberry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$hua=$_SERVER['HTTP_USER_AGENT'];
+$iphone = strpos($hua,"iPhone");
+$android = strpos($hua,"Android");
+$webos = strpos($hua,"webOS");
+$ipod = strpos($hua,"iPod");
 if ($iphone || $android || $webos || $ipod || $bberry)
 {
 echo "blabla";
 }
+
+
+// Sıkıştırılmış
+$hua=$_SERVER['HTTP_USER_AGENT'];$iphone=strpos($hua,"iPhone");$android=strpos($hua,"Android");$webos=strpos($hua,"webOS");$ipod=strpos($hua,"iPod");if($iphone||$android||$webos||$ipod||$bberry){echo"blabla";}

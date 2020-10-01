8256f1d62bda7490c32976329e38bd5ee07b84e6
Uğur KILCI <ugurbocugu8@gmail.com>
2020-10-01 21:49:33 +0300

    Strtolower Türkçe Kararter Sorunu Çözümü

---
+++ strtolower-turkce.php
@@ -0,0 +1,9 @@
+<?php
+
+function strtolower_turkce($metin){
+    $bul 	= array("I","Ğ","Ü","Ş","İ","Ö","Ç");
+    $degis  = array("ı","ğ","ü","ş","i","ö","ç");
+    $metin	= str_replace($bul, $degis, $metin);
+    $metin	= strtolower($metin);
+    return $metin;
+}

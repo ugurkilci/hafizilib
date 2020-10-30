54ab967f4b3b67e615a3a46d588ac501907a5b37
Uğur KILCI <ugurbocugu8@gmail.com>
2020-10-30 22:55:47 +0300

    Update and rename verilerin-toplami-fonsiyonu.php to verilerin-toplami-fonsiyonu-say.php

--- verilerin-toplami-fonsiyonu-say.php
+++ verilerin-toplami-fonsiyonu-say.php
@@ -2,7 +2,7 @@
 
 // Veritabanındaki verilerin toplamını gösteren fonksiyon
 
-function sayy($a){
+function say($a){
  include 'ayar.php'; // ayar dosyası
  $sorgu = $db->prepare("SELECT COUNT(*) FROM icerik WHERE kate=?");
  $sorgu->execute(array($a));

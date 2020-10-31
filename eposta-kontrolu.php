28136c3c7ecf0c50965bbc5fd4b5831779bd8428
Uğur KILCI <ugurbocugu8@gmail.com>
2020-10-31 13:13:41 +0300

    Update eposta-kontrolu.php

--- eposta-kontrolu.php
+++ eposta-kontrolu.php
@@ -3,8 +3,8 @@
 // Güvenlik için önemli. Eposta gerçek mi değil mi kontrol eder.
 
 $eposta = "ugurbocugu8@gmail.com";
-if ( filter_var($eposta, FILTER_VALIDATE_EMAIL) ){ 
-   print 'eposta gecerli bro';
-} else {
-   print 'eposta gecersiz! sen hayirdir?';
+if (filter_var($eposta, FILTER_VALIDATE_EMAIL)){ // :)
+
+} else { // :(
+
 }

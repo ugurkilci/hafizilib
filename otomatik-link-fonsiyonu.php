afe9bf3c7d5ea7b65e4aa53c0c827406e3673645
Uğur KILCI <ugurbocugu8@gmail.com>
2021-05-24 12:26:00 +0300

    Update otomatik-link-fonsiyonu.php

--- otomatik-link-fonsiyonu.php
+++ otomatik-link-fonsiyonu.php
@@ -2,10 +2,20 @@
 
 // Bir yazı içinde link varsa onu tıklanabilir hale getiriyor. Kullanışlı.
 
-function otolink($text) {
-    $pattern = '/(((http[s]?:\/\/(.+(:.+)?@)?)|(www\.))[a-z0-9](([-a-z0-9]+\.)*\.[a-z]{2,})?\/?[a-z0-9.,_\/~#&=:;%+!?-]+)/is';
-    $text = preg_replace($pattern, ' $1', $text);
-    // fix URLs without protocols
-    $text = preg_replace('/href="www/', 'href="http://www', $text);
-    return $text;
-}
+function otolink($veri, $ozellikler=array()) {
+    $ekOzellik  = "";
+    $veri       = ' ' . $veri;
+    foreach ($ozellikler as $ozellik => $deger) {
+  $ekOzellik .= " {$ozellik}=\"{$deger}\"";
+    }
+    $veri = preg_replace(
+       '`([^"=\'>])((http|https|ftp)://[^\s<]+[^\s<\.)])`i',
+       '$1<a href="$2"'.$ekOzellik.'>$2</a>',
+       $veri
+    );
+    $veri = substr($veri, 1);
+    return $veri;
+ }
+
+ $str = 'test 123 http://ugurkilci.com global cap cup';
+ echo otolink($str, array("target"=>"_blank","rel"=>"nofollow noopener noreferrer"));

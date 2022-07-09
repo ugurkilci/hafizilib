10ca14f6cb49a2d16ab8d47a828070a878eb2163
Uğur KILCI <ugurbocugu8@gmail.com>
2022-07-09 14:57:14 +0300

    Update veriekleme.php

--- veriekleme.php
+++ veriekleme.php
@@ -13,6 +13,10 @@ $dataAdd -> execute([
 
 if ( $dataAdd ) {
     echo '<p class="alert alert-success">Successfully added. :)</p>';
+    
+    header("REFRESH:1;URL=index.php");
 } else {
     echo '<p class="alert alert-danger">Oops, you encountered an error while adding it. Please try again. :/</p>';
+    
+    header("REFRESH:1;URL=index.php");
 }

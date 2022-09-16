c219c8985822819ed04323ed9ac9695353f08fd8
Uğur KILCI <ugurbocugu8@gmail.com>
2022-09-16 11:20:13 +0300

    Update PHP-zip.php

--- PHP-zip.php
+++ PHP-zip.php
@@ -7,16 +7,16 @@ $zip->addFile('a.txt', 'yeniadi.txt');
 $zip->addFile('b.txt', 'yeniadib.txt');
 $zip->close();
 
-// Şifreli Zip Yapmak
-$zip = new ZIPArchive();
-$zip->open("dosyaadi.zip", ZIPARCHIVE::CREATE);
-$zip->addFile('a.txt', 'yeniadi.txt');
-$zip->setEncryptionName('dosyaadi.zip', ZipArchive::EM_AES_256, 'şifre');
-$zip->close();
+// // Şifreli Zip Yapmak
+// $zip = new ZIPArchive();
+// $zip->open("dosyaadi.zip", ZIPARCHIVE::CREATE);
+// $zip->addFile('a.txt', 'yeniadi.txt');
+// $zip->setEncryptionName('dosyaadi.zip', ZipArchive::EM_AES_256, 'şifre');
+// $zip->close();
 
-// Zipten dışarı çıkarmak
-$zip = new ZIPArchive();
-$zip->open("dosyaadi.zip");
-$zip->setPassword('şifre');
-$zip->extractTo('çıkarılacak klasör');
-$zip->close();
+// // Zipten dışarı çıkarmak
+// $zip = new ZIPArchive();
+// $zip->open("dosyaadi.zip");
+// $zip->setPassword('şifre');
+// $zip->extractTo('çıkarılacak klasör');
+// $zip->close();

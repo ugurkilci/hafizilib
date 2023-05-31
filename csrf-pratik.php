bd6cc8d53d187f07438c134b39af21616bfbca5f
Uğur KILCI <ugurbocugu8@gmail.com>
2023-05-31 20:25:42 +0300

    Create csrf-pratik.php

---
+++ csrf-pratik.php
@@ -0,0 +1,13 @@
+<?php
+
+function csrf()
+{
+    // Form içine yazın
+    echo '<input type="hidden" name="_token" value="'.$_SESSION["_token"].'">';
+}
+
+function csrfControl()
+{
+    // Post içinde kullanınız. Bilhassa en tepesinde.
+    if ($_POST["_token"] !== $_SESSION["_token"]) { die('CSRF Token!'); }
+}
